ba548b486b04f655aef99f1d7050ae7bf36ab399
ming-ming tran <ming-ming.tran+1789489855@dbtlabsworkshop.com>
2026-09-15 18:03:45 +0000

    exercises 3+4

--- tests/generic/type_decimal.sql
+++ tests/generic/type_decimal.sql
@@ -1,16 +1,8 @@
 {% test type_decimal(model, column_name) %}
 
-with validation as (
-
-    select
-        {{ column_name }} as column_name
-
-    from {{ model }}
-
-)
-
-select *
-from validation
-where typeof(column_name) <> 'DECIMAL'
+   select
+       {{ column_name }}
+   from {{ model }}
+   where typeof({{ column_name }}) <> 'DECIMAL'
 
 {% endtest %}
